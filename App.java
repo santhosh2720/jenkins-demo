0bc9061739ad2a79c7319663dfcee7765af0e39e
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-01 18:30:24 +0530

    Initial Java CI/CD commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java CI/CD Application Started...");
+        while(true) {
+            Thread.sleep(5000);
+            System.out.println("hello from Java CI/CD pipepline");
+        }
+    }
+}
